eab9f1ed1a2efd7b05aa92c3694e608a756c1ebd
JianW <49907914+v1212@users.noreply.github.com>
2023-09-06 23:20:25 +0800

    Update image.js

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,18 +1,6 @@
-import axios from 'axios'
-
-export default async function handler(req, res) {
-  try {
-    const response = await axios.get('https://picsum.photos/200')
-    const imageContent = response.data.toString('base64')
-
-    res.status(200).send({
-      headers: {
-        'Content-Type': 'image/jpeg'
-      },
-      body: imageContent
-    })
-  } catch (error) {
-    console.error(error)
-    res.status(500).send('Error getting image')
-  }
-}
+export default async (req, res) => {
+  const imageBuffer = await fetch('https://source.unsplash.com/random/800x600').then((res) => res.buffer());
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'image/jpeg');
+  res.end(imageBuffer);
+};
